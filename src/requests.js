1cc3db60d26bb6c35d5ece7febc1cfaccecda1e8
Ram Iyer <riyer@teladoc.com>
2019-02-01 14:55:49 -0500

    adding request.js the consists of all graphql api calls, removing hardcoding from JobBoard and JobDetail

---
+++ src/requests.js
@@ -0,0 +1,44 @@
+const endpointURL = 'http://localhost:9000/graphql';
+
+export async function loadJobs() {
+  const response = await fetch(endpointURL, {
+    method: 'POST',
+    headers: { 'content-type': 'application/json'},
+    body: JSON.stringify({
+      query: `{
+        jobs {
+          id
+          title
+          company {
+            id
+            name
+          }
+        }
+      }`
+    })
+  });
+  const responseBody = await response.json();
+  return responseBody.data.jobs;
+}
+
+export async function getJob(id) {
+  const response = await fetch(endpointURL, {
+    method: 'POST',
+    headers: { 'content-type': 'application/json'},
+    body: JSON.stringify({
+      query: `query JobQuery($id: ID!) {
+        job(id: $id) {
+          id
+          title
+          description
+          company {
+            id
+            name
+          }
+        }
+      }`, variables: {id}
+    })
+  });
+  const responseBody = await response.json();
+  return responseBody.data.job;
+}
