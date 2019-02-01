d73540808f3e94e513b61deb9edadfd497f01b6d
Ram Iyer <riyer@teladoc.com>
2019-02-01 15:10:54 -0500

    extracting into graphqlRequest to make it DRY

--- src/requests.js
+++ src/requests.js
@@ -1,44 +1,42 @@
 const endpointURL = 'http://localhost:9000/graphql';
 
 export async function loadJobs() {
-  const response = await fetch(endpointURL, {
-    method: 'POST',
-    headers: { 'content-type': 'application/json'},
-    body: JSON.stringify({
-      query: `{
-        jobs {
-          id
-          title
-          company {
-            id
-            name
-          }
-        }
-      }`
-    })
-  });
-  const responseBody = await response.json();
-  return responseBody.data.jobs;
+  const query = `{
+    jobs {
+      id
+      title
+      company {
+        id
+        name
+      }
+    }
+  }`
+  const {jobs} = await graphqlRequest(query);
+  return jobs;
 }
 
 export async function getJob(id) {
+  const query = `query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      description
+      company {
+        id
+        name
+      }
+    }
+  }`;
+  const {job} = await graphqlRequest(query, {id});
+  return job;
+}
+
+async function graphqlRequest(query, variables={}) {
   const response = await fetch(endpointURL, {
     method: 'POST',
     headers: { 'content-type': 'application/json'},
-    body: JSON.stringify({
-      query: `query JobQuery($id: ID!) {
-        job(id: $id) {
-          id
-          title
-          description
-          company {
-            id
-            name
-          }
-        }
-      }`, variables: {id}
-    })
+    body: JSON.stringify({query, variables})
   });
   const responseBody = await response.json();
-  return responseBody.data.job;
+  return responseBody.data;
 }
