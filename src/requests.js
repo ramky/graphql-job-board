ed71c14080f4e8454cffd714d11e5ee6aba62627
Ram Iyer <ramky.iyer@gmail.com>
2019-03-01 20:59:26 -0500

    eliminating unnecessary getJob graphql call after mutation using apollo cache

--- src/requests.js
+++ src/requests.js
@@ -39,21 +39,21 @@ export async function loadJobs() {
   return jobs;
 }
 
-export async function getJob(id) {
-  const query = gql`query
-    JobQuery($id: ID!) {
-      job(id: $id) {
-        id
-        title
-        description
-        company {
-          id
-          name
-        }
-      }
+const jobQuery = gql`query
+JobQuery($id: ID!) {
+  job(id: $id) {
+    id
+    title
+    description
+    company {
+      id
+      name
     }
-  `;
-  const {data: {job}} = await client.query({query, variables: {id}})
+  }
+}`;
+
+export async function getJob(id) {
+  const {data: {job}} = await client.query({query: jobQuery, variables: {id}})
   return job;
 }
 
@@ -81,6 +81,7 @@ export async function createJob(input) {
       job: createJob(input: $input) {
         id
         title
+        description
         company {
           id
           name
@@ -88,6 +89,16 @@ export async function createJob(input) {
       }
     }
   `;
-  const {data: {job}} = await client.mutate({mutation, variables: {input}})
+  const {data: {job}} = await client.mutate({
+    mutation,
+    variables: {input},
+    update: (cache, {data}) => {
+      cache.writeQuery({
+        query: jobQuery,
+        variables: {id: data.job.id},
+        data
+      })
+    }
+  })
   return job;
 }
